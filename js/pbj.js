1a5590d9d8281c2dfee83da4555b69339fe1edd8
Cyle Carlson <jacerider@gmail.com>
2013-06-05 12:08:11 -0500

    Remove all console.logs.

--- js/pbj.js
+++ js/pbj.js
@@ -3,14 +3,14 @@
 Drupal.behaviors.pbj = {
 	attach: function(context, settings) {
 		if (!Drupal.settings.pbj) return;
-		
+
 		Drupal.behaviors.pbj.editorSettings = Drupal.settings.pbj.settings;
 
 		/**
 		* Bind the change event to all text format select lists.
 		*/
 		$('div.text-format-wrapper fieldset.filter-wrapper select.filter-list').once('pbj').bind('change', function(e) {
-			console.log('hit');
+			//console.log('hit');
 			var $textFormatWrapper = $(this).parents('div.text-format-wrapper:first');
 			Drupal.behaviors.pbj.pbjify($textFormatWrapper);
 		});
@@ -51,7 +51,7 @@ Drupal.behaviors.pbj = {
 				$('#pbj-position').val(value);
 			}
 		});
-		
+
 	},
 
 	editorSettings: {},
@@ -60,7 +60,7 @@ Drupal.behaviors.pbj = {
 		// The select list for chosing the text format that will be used.
 		var $filterSelector = $textFormatWrapper.find('select.filter-list');
 		var settings = Drupal.behaviors.pbj.editorSettings;
-			
+
 		// Checks if the currently selected filter is one that uses the editor.
 		if ($.inArray($filterSelector.val(), settings.text_formats) != -1) {
 			$('.wmd-preview').show();
@@ -69,7 +69,7 @@ Drupal.behaviors.pbj = {
    		var editor1 = new Markdown.Editor(converter1);
    		editor1.run();
 		} else { // Show the textarea.
-			console.log('nope');
+			//console.log('nope');
 			$('.wmd-preview').hide();
 			$('.wmd-button-bar').html('');
 			$('.wmd-input-active textarea').css('height','');
